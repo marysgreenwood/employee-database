81cdf6ddc9f32bff6bdff8f0bfd329c277f8a758
marysgreenwood <marysgreenwood@gmail.com>
2023-10-11 14:51:34 -0400

    updated connection and gitignore files

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+require("dotenv").config();
 
- 
 db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: 'Saxony1066',
-      database: 'employee_db'
-    }, 
-    console.log(`connected to database`)
-)
-    
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`connected to database`)
+);
 
-    module.exports= db
+module.exports = db;
